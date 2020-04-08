e925ddc2117eb7a966aa0c09944b591a9815a9e8
namjin <domino8788@naver.com>
2020-04-08 17:59:40 +0900

    #51 스크롤 범위 제한 기능구현 완료

--- app/src/main/java/com/domino/skhumap/view/CustomMapView.kt
+++ app/src/main/java/com/domino/skhumap/view/CustomMapView.kt
@@ -5,11 +5,15 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.appcompat.widget.AppCompatImageView
 import com.domino.skhumap.R
+import kotlin.math.abs
 
 class CustomMapView(context:Context, private val attrs: AttributeSet?, private val defStyleAttr:Int): AppCompatImageView(context, attrs, defStyleAttr) {
 
     private var downX = 0f
     private var downY = 0f
+
+    private var imageWidth = 0
+    private var imageHeight = 0
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -26,23 +30,41 @@ class CustomMapView(context:Context, private val attrs: AttributeSet?, private v
      */
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
+            /* 터치 시 발생하는 액션 */
             MotionEvent.ACTION_DOWN -> {
-                downX = event.x
-                downY = event.y
+                downX = event.x //터치할때 x좌표를 저장한다.
+                downY = event.y //터치할때 y좌표를 저장한다.
             }
+            /* 터치 후 드래그 시 발생하는 액션 */
             MotionEvent.ACTION_MOVE -> {
-                val moveX = event.x
-                val moveY = event.y
-                val distanceX = downX-moveX
-                val distanceY = downY-moveY
+                val moveX = event.x //드래그할때 x좌표를 저장한다,
+                val moveY = event.y //드래그할때 y좌표를 저장한다.
+                val distanceX = downX-moveX //터치할때의 x좌표와 드래그할때 x좌표 사이 거리를 계산한다.
+                val distanceY = downY-moveY //터치할때의 y좌표와 드래그할때 y좌표 사이 거리를 계산한다.
 
-                this.scrollBy(distanceX.toInt(), distanceY.toInt())
+                this.scrollBy(distanceX.toInt(), distanceY.toInt()) //드래그할때 x, y축으로 이동한 만큼 스크롤을 이동시킨다.
 
-                downX = moveX
-                downY = moveY
+                downX = moveX //기존 드래그할때 x좌표의 값을 보관한다.
+                downY = moveY //기존 드래그할때 y좌표의 값을 보관한다.
             }
+            /* 드래그가 끝나고 터치를 떼면 발생하는 액션 */
             MotionEvent.ACTION_UP -> {
+                var overX = 0f //이미지 범위를 x축으로 오버한 거리
+                var overY = 0f //이미지 범위를 y축으로 오버한 거리
 
+                if(abs(scrollX) > imageWidth/3) { //스크롤이 이미지 width의 1/3 을 오버하면
+                    if (scrollX > 0) //x>0 방향으로 스크롤 햇을때 x축 오버거리를 계산한다.
+                        overX = -(scrollX - imageWidth / 3 + 1).toFloat()
+                    else //x<0 방향으로 스크롤 했을때 y축 오버거리를 계산한다.
+                        overX = -(scrollX + imageWidth / 3 - 1).toFloat()
+                }
+                if(abs(scrollY) > imageHeight/3) { //스크롤이 이미지 height의 1/3을 오버하면
+                    if (scrollY > 0) //y>0 방향으로 스크롤 했을때 y축 오버거리를 계산한다.
+                        overY = -(scrollY - imageHeight / 3 + 1).toFloat()
+                    else //y<0 방향으로 스크롤 했을때 y축 오버거리를 계산한다.
+                        overY = -(scrollY + imageHeight / 3 - 1).toFloat()
+                }
+                scrollBy(overX.toInt(), overY.toInt()) //오버한 거리 만큼 스크롤을 되돌린다.
             }
         }
         return true
@@ -58,10 +80,15 @@ class CustomMapView(context:Context, private val attrs: AttributeSet?, private v
     fun adjustAttrs(attrs:AttributeSet, defStyle:Int) =
         context.obtainStyledAttributes(attrs,
             R.styleable.CustomMapView, defStyleAttr, 0).let {
-            setImageResource(it.getResourceId(
+            val resId = it.getResourceId(
                 R.styleable.CustomMapView_mapSrc,
                 R.drawable.temp_campus_map
-            ))
+            )
+            setImageResource(resId)
+            resources.getDrawable(resId).run {
+                imageWidth = intrinsicWidth
+                imageHeight = intrinsicWidth
+            }
             scaleType = ScaleType.CENTER
             it.recycle()
         }
