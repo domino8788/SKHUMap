acfb42d563f79f8e206e8916b2c8c94fd39ef86a
namjin <domino8788@naver.com>
2020-04-08 15:08:49 +0900

    #23 #46 #49 상하 좌우 대각선 스크롤 이미지뷰 구현 완료

--- app/src/main/java/com/domino/skhumap/view/CustomMapView.kt
+++ app/src/main/java/com/domino/skhumap/view/CustomMapView.kt
@@ -3,75 +3,49 @@ package com.domino.skhumap.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
-import android.widget.HorizontalScrollView
-import android.widget.ImageView
-import android.widget.ScrollView
+import androidx.appcompat.widget.AppCompatImageView
 import com.domino.skhumap.R
-import kotlinx.android.synthetic.main.custom_map_view.view.*
 
-class CustomMapView(context:Context, private val attrs: AttributeSet?, private val defStyleAttr:Int): ScrollView(context, attrs, defStyleAttr) {
+class CustomMapView(context:Context, private val attrs: AttributeSet?, private val defStyleAttr:Int): AppCompatImageView(context, attrs, defStyleAttr) {
 
-    private var mx = 0f
-    private var my = 0f
-    private val mapView: View = inflate(
-        context,
-        R.layout.custom_map_view, this
-    )
-    private val vScroll: ScrollView by lazy { mapView.scroll_Vertical }
-    private val hScroll: HorizontalScrollView by lazy { mapView.scroll_Horizontal }
-    private val imageView: ImageView by lazy { mapView.img_map }
+    private var downX = 0f
+    private var downY = 0f
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
     init {
         attrs?.let { adjustAttrs(attrs!!, defStyleAttr) }
-//        hScroll.setOnTouchListener { v, event ->
-//            var curX: Float
-//            var curY: Float
-//
-//            when (event.action) {
-//                MotionEvent.ACTION_DOWN -> {
-//                    mx = event.x
-//                    my = event.y
-//                }
-//                MotionEvent.ACTION_MOVE -> {
-//                    curX = event.x
-//                    curY = event.y
-//                    img_map.scrollBy(((mx - curX)*1.5).toInt(), ((my - curY)*1.5).toInt())
-//                    mx = curX
-//                    my = curY
-//                }
-//                MotionEvent.ACTION_UP -> {
-//
-//                }
-//            }
-//            true
-//        }
-//        vScroll.setOnTouchListener { v, event ->
-//            var curX: Float
-//            var curY: Float
-//
-//            when (event.action) {
-//                MotionEvent.ACTION_DOWN -> {
-//                    mx = event.x
-//                    my = event.y
-//                }
-//                MotionEvent.ACTION_MOVE -> {
-//                    curX = event.x
-//                    curY = event.y
-//                    img_map.scrollBy(((mx - curX)*1.5).toInt(), ((my - curY)*1.5).toInt())
-//                    mx = curX
-//                    my = curY
-//                }
-//                MotionEvent.ACTION_UP -> {
-//
-//                }
-//            }
-//            true
-//        }
+    }
+
+    /**
+     * Touch 이벤트 발생
+     * @param event 모션 이벤트 정보가 담긴 객체
+     * @author domino
+     * @since 2020.4.8.
+     */
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                downX = event.x
+                downY = event.y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val moveX = event.x
+                val moveY = event.y
+                val distanceX = downX-moveX
+                val distanceY = downY-moveY
+
+                this.scrollBy(distanceX.toInt(), distanceY.toInt())
+
+                downX = moveX
+                downY = moveY
+            }
+            MotionEvent.ACTION_UP -> {
+
+            }
+        }
+        return true
     }
 
     /**
@@ -84,35 +58,12 @@ class CustomMapView(context:Context, private val attrs: AttributeSet?, private v
     fun adjustAttrs(attrs:AttributeSet, defStyle:Int) =
         context.obtainStyledAttributes(attrs,
             R.styleable.CustomMapView, defStyleAttr, 0).let {
-            imageView.setImageResource(it.getResourceId(
+            setImageResource(it.getResourceId(
                 R.styleable.CustomMapView_mapSrc,
                 R.drawable.temp_campus_map
             ))
-
+            scaleType = ScaleType.CENTER
             it.recycle()
         }
-
-    override fun onInterceptTouchEvent(event: MotionEvent?): Boolean {
-        var curX: Float
-        var curY: Float
-
-            when (event!!.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    mx = event.x
-                    my = event.y
-                }
-                MotionEvent.ACTION_MOVE -> {
-                    curX = event.x
-                    curY = event.y
-                    img_map.scrollBy(((mx - curX)*1.5).toInt(), ((my - curY)*1.5).toInt())
-                    mx = curX
-                    my = curY
-                }
-                MotionEvent.ACTION_UP -> {
-
-                }
-            }
-        return super.onInterceptTouchEvent(event)
-    }
 
 }
