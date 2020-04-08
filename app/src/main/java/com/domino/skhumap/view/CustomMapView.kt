26e408b726f83ab768a991b144332d86bc1b32f8
namjin <domino8788@naver.com>
2020-04-08 12:43:00 +0900

    #23 #46 #49 다른 방안으로 구현할 것을 권고

--- app/src/main/java/com/domino/skhumap/view/CustomMapView.kt
+++ app/src/main/java/com/domino/skhumap/view/CustomMapView.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.HorizontalScrollView
 import android.widget.ImageView
 import android.widget.ScrollView
 import com.domino.skhumap.R
 import kotlinx.android.synthetic.main.custom_map_view.view.*
 
-class CustomMapView(context:Context, private val attrs: AttributeSet?, private val defStyleAttr:Int): ScrollView(context, attrs, defStyleAttr){
+class CustomMapView(context:Context, private val attrs: AttributeSet?, private val defStyleAttr:Int): ScrollView(context, attrs, defStyleAttr) {
 
     private var mx = 0f
     private var my = 0f
@@ -27,50 +28,50 @@ class CustomMapView(context:Context, private val attrs: AttributeSet?, private v
 
     init {
         attrs?.let { adjustAttrs(attrs!!, defStyleAttr) }
-        hScroll.setOnTouchListener { v, event ->
-            var curX: Float
-            var curY: Float
-
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    mx = event.x
-                    my = event.y
-                }
-                MotionEvent.ACTION_MOVE -> {
-                    curX = event.x
-                    curY = event.y
-                    img_map.scrollBy(((mx - curX)*1.5).toInt(), ((my - curY)*1.5).toInt())
-                    mx = curX
-                    my = curY
-                }
-                MotionEvent.ACTION_UP -> {
-
-                }
-            }
-            true
-        }
-        vScroll.setOnTouchListener { v, event ->
-            var curX: Float
-            var curY: Float
-
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    mx = event.x
-                    my = event.y
-                }
-                MotionEvent.ACTION_MOVE -> {
-                    curX = event.x
-                    curY = event.y
-                    img_map.scrollBy(((mx - curX)*1.5).toInt(), ((my - curY)*1.5).toInt())
-                    mx = curX
-                    my = curY
-                }
-                MotionEvent.ACTION_UP -> {
-
-                }
-            }
-            true
-        }
+//        hScroll.setOnTouchListener { v, event ->
+//            var curX: Float
+//            var curY: Float
+//
+//            when (event.action) {
+//                MotionEvent.ACTION_DOWN -> {
+//                    mx = event.x
+//                    my = event.y
+//                }
+//                MotionEvent.ACTION_MOVE -> {
+//                    curX = event.x
+//                    curY = event.y
+//                    img_map.scrollBy(((mx - curX)*1.5).toInt(), ((my - curY)*1.5).toInt())
+//                    mx = curX
+//                    my = curY
+//                }
+//                MotionEvent.ACTION_UP -> {
+//
+//                }
+//            }
+//            true
+//        }
+//        vScroll.setOnTouchListener { v, event ->
+//            var curX: Float
+//            var curY: Float
+//
+//            when (event.action) {
+//                MotionEvent.ACTION_DOWN -> {
+//                    mx = event.x
+//                    my = event.y
+//                }
+//                MotionEvent.ACTION_MOVE -> {
+//                    curX = event.x
+//                    curY = event.y
+//                    img_map.scrollBy(((mx - curX)*1.5).toInt(), ((my - curY)*1.5).toInt())
+//                    mx = curX
+//                    my = curY
+//                }
+//                MotionEvent.ACTION_UP -> {
+//
+//                }
+//            }
+//            true
+//        }
     }
 
     /**
@@ -90,4 +91,28 @@ class CustomMapView(context:Context, private val attrs: AttributeSet?, private v
 
             it.recycle()
         }
+
+    override fun onInterceptTouchEvent(event: MotionEvent?): Boolean {
+        var curX: Float
+        var curY: Float
+
+            when (event!!.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    mx = event.x
+                    my = event.y
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    curX = event.x
+                    curY = event.y
+                    img_map.scrollBy(((mx - curX)*1.5).toInt(), ((my - curY)*1.5).toInt())
+                    mx = curX
+                    my = curY
+                }
+                MotionEvent.ACTION_UP -> {
+
+                }
+            }
+        return super.onInterceptTouchEvent(event)
+    }
+
 }
