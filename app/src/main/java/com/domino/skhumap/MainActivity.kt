8171f86d5db917ae788a1b359b9cdda62d302b1e
namjin <domino8788@naver.com>
2020-04-09 13:02:07 +0900

    #9 MapView 연동 완료

--- app/src/main/java/com/domino/skhumap/MainActivity.kt
+++ app/src/main/java/com/domino/skhumap/MainActivity.kt
@@ -2,11 +2,28 @@ package com.domino.skhumap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.UiThread
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.main_map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.main_map, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
+    }
+
+    @UiThread
+    override fun onMapReady(p0: NaverMap) {
+
     }
 }
