13c5c160f519ee85b15b3aa9925a51bc12d16506
namjin <domino8788@naver.com>
2020-04-09 23:52:01 +0900

    #9 지도 api에 캠퍼스 지도 이미지 표시 완료

--- app/src/main/java/com/domino/skhumap/NaverMapFragment.kt
+++ app/src/main/java/com/domino/skhumap/NaverMapFragment.kt
@@ -1,20 +1,27 @@
 package com.domino.skhumap
 
+import android.graphics.Color
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorInt
 import androidx.annotation.UiThread
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
+import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.GroundOverlay
+import com.naver.maps.map.overlay.LocationOverlay
+import com.naver.maps.map.overlay.OverlayImage
 
 class NaverMapFragment : Fragment(), OnMapReadyCallback {
     lateinit var naverMap:NaverMap
+    val campusGroudOverlay by lazy { GroundOverlay() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,8 +40,23 @@ class NaverMapFragment : Fragment(), OnMapReadyCallback {
     @UiThread
     override fun onMapReady(nMap: NaverMap) {
         this.naverMap = nMap
-        naverMap.extent = LatLngBounds(LatLng(37.486033, 126.823969), LatLng(37.489835, 126.827264))
-        naverMap.minZoom = 16.5
+        naverMap.run {
+            extent = LatLngBounds(LatLng(37.486033, 126.823969), LatLng(37.489835, 126.827264))
+            minZoom = 16.5
+            mapType = NaverMap.MapType.Terrain
+            uiSettings.run {
+                isCompassEnabled = false
+                isZoomControlEnabled = false
+                isScaleBarEnabled = false
+                isRotateGesturesEnabled = false
+                isTiltGesturesEnabled = false
+            }
+            cameraPosition = CameraPosition(LatLng(37.487600, 126.825643), 16.5, 0.0, 67.5)
+        }
+        campusGroudOverlay.run {
+            image = OverlayImage.fromResource(R.drawable.campus)
+            bounds = LatLngBounds(LatLng(37.486427880037326, 126.82376783058442), LatLng(37.48854961512034, 126.82754956984687))
+            map = nMap
+        }
     }
-
 }
