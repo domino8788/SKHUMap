ef833c6b49f93a193b4c3dc43b543aa4733ba2c4
namjin <domino8788@naver.com>
2020-04-09 16:05:03 +0900

    #55 터치한 지점의 위/경도 값 표시 기능 구현

--- app/src/main/java/com/domino/skhumap/NaverMapFragment.kt
+++ app/src/main/java/com/domino/skhumap/NaverMapFragment.kt
@@ -1,8 +1,10 @@
 package com.domino.skhumap
 
+import android.graphics.PointF
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,8 +14,9 @@ import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import kotlinx.android.synthetic.main.fragment_map.*
 
-class NaverMapFragment : Fragment(), OnMapReadyCallback {
+class NaverMapFragment : Fragment(), OnMapReadyCallback, NaverMap.OnMapClickListener {
     lateinit var naverMap:NaverMap
 
     override fun onCreateView(
@@ -28,13 +31,23 @@ class NaverMapFragment : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as MapFragment?
         mapFragment?.getMapAsync(this)
+
+        txt_current_touch_latlng
     }
 
     @UiThread
     override fun onMapReady(nMap: NaverMap) {
-        this.naverMap = nMap
+        naverMap = nMap
         naverMap.extent = LatLngBounds(LatLng(37.486033, 126.823969), LatLng(37.489835, 126.827264))
         naverMap.minZoom = 16.5
+
+        naverMap.setOnMapClickListener(this)
+
+    }
+
+    @UiThread
+    override fun onMapClick(point: PointF, latlng: LatLng) {
+        txt_current_touch_latlng.text = latlng.toString()
     }
 
 }
