d6077d8141e0a3ed5e6eb4bba3fdef4c8c66741d
namjin <domino8788@naver.com>
2020-04-17 13:49:44 +0900

    #67 에러해결

--- app/src/main/java/com/domino/skhumap/Department.kt
+++ app/src/main/java/com/domino/skhumap/Department.kt
@@ -6,6 +6,18 @@ import com.google.firebase.firestore.GeoPoint
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.overlay.Marker
 
-data class Department(val id:String, val name:String, val location:GeoPoint)
+data class Department(val id:String="", val name:String="", val location:GeoPoint?=null, val type:DocumentReference?=null,
+                      var marker:Marker?=null)
 {
+
+    init{
+        marker = Marker().apply {
+            captionText = "$id  $name"
+            position = LatLng(location!!.latitude, location!!.longitude)
+            map = naverMap }
+    }
+
+    fun removeMarker(){
+        marker?.map = null
+    }
 }
