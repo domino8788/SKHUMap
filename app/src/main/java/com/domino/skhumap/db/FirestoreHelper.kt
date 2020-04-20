e7e69d8845a1075c3b2b3b855543f95bc293f62a
namjin <domino8788@naver.com>
2020-04-21 00:29:50 +0900

    #8 조회 후 콜백함수를 실행하도록 수정

--- app/src/main/java/com/domino/skhumap/db/FirestoreHelper.kt
+++ app/src/main/java/com/domino/skhumap/db/FirestoreHelper.kt
@@ -2,9 +2,11 @@ package com.domino.skhumap.db
 
 import android.content.Context
 import com.domino.skhumap.Facility
+import com.domino.skhumap.manager.MapManager
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.naver.maps.map.overlay.Marker
 
 object FirestoreHelper {
 
@@ -12,10 +14,12 @@ object FirestoreHelper {
     private const val COLLECTION_FACILITIES = "facilities"
     val campusReference by lazy { db.collection(COLLECTION_FACILITIES) }
 
-    fun queryPullDriven(queryTarget: CollectionReference) {
+    fun queryPullDriven(queryTarget: CollectionReference, callback: ((facilities:MutableList<Facility>) -> Unit)?) {
         queryTarget.get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                task.result!!.toObjects(Facility::class.java)
+                MapManager.run {
+                    task.result!!.toObjects(Facility::class.java).also { (callback)?.let { func -> func(it) } }
+                }
             }
         }
     }
