5fb0b8818015020910fd8c893abeb112ee7d073f
namjin <domino8788@naver.com>
2020-04-20 21:37:38 +0900

    #62 firestore 연동을 위한 build.gradle 수정

--- app/src/main/java/com/domino/skhumap/activity/MainActivity.kt
+++ app/src/main/java/com/domino/skhumap/activity/MainActivity.kt
@@ -3,11 +3,14 @@ package com.domino.skhumap.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.domino.skhumap.R
+import com.domino.skhumap.db.FirestoreHelper
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirestoreHelper.init(this)
+
     }
 }
