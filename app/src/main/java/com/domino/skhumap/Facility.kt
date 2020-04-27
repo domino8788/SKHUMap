69bc8a6af2d6957651ad292ab73d5ea833c6d38d
namjin <domino8788@naver.com>
2020-04-27 18:51:48 +0900

    #65 #19 #20 name 필드 변경사항 반영 및 실내지도 시설 관련 TYPE 추가

--- app/src/main/java/com/domino/skhumap/Facility.kt
+++ app/src/main/java/com/domino/skhumap/Facility.kt
@@ -2,7 +2,7 @@ package com.domino.skhumap
 
 import android.os.Parcel
 import android.os.Parcelable
-import com.domino.skhumap.MapManager.naverMap
+import com.domino.skhumap.manager.MapManager
 import com.google.firebase.firestore.*
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.overlay.Marker
@@ -11,7 +11,7 @@ import com.naver.maps.map.overlay.OverlayImage
 @IgnoreExtraProperties
 data class Facility(
     @DocumentId var id: String = "",
-    @PropertyName("name") var name: String = "",
+    @PropertyName("name") var name: List<String>? = null,
     @PropertyName("location") var location: GeoPoint? = null,
     @PropertyName("type") var type: Int = 0,
     @PropertyName("info") var info: HashMap<String, Any>? = null
@@ -23,11 +23,22 @@ data class Facility(
     constructor(parcel: Parcel) : this() {
         parcel.run {
             id = readString()!!
-            name = readString()!!
+            readList(name, Facility.javaClass.classLoader)!!
             location = GeoPoint(parcel.readDouble(), parcel.readDouble())
             type = readInt()
             info = readSerializable() as HashMap<String, Any>
         }
+    }
+
+    enum class TYPE(val id:Int, val icon:Int){
+        DEPARTMENT(0, R.drawable.ic_apartment_black_24dp),
+        LECTUREROOM(1, R.drawable.ic_apartment_black_24dp),
+        TOILET(2, R.drawable.ic_apartment_black_24dp),
+        ADMINISTRATION(3, R.drawable.ic_apartment_black_24dp),
+        PROJECTROOM(4, R.drawable.ic_apartment_black_24dp),
+        PROFESSORROOM(5, R.drawable.ic_apartment_black_24dp),
+        DOOR(6, R.drawable.ic_apartment_black_24dp),
+        INFO(7, R.drawable.ic_apartment_black_24dp)
     }
 
     fun addMarker(): Marker? = Marker().apply {
@@ -35,13 +46,12 @@ data class Facility(
         position = LatLng(location!!.latitude, location!!.longitude)
         icon = OverlayImage.fromResource(
             when (type) {
-                MapManager.Type.DEPARTMENT.id -> MapManager.Type.DEPARTMENT.icon
-                else -> MapManager.Type.DEPARTMENT.icon
+                TYPE.DEPARTMENT.id -> TYPE.DEPARTMENT.icon
+                else -> TYPE.DEPARTMENT.icon
             }
         )
         isHideCollidedSymbols = true
-
-        map = naverMap
+        map = MapManager.naverMap
         marker = this
     }
 
@@ -52,7 +62,7 @@ data class Facility(
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.run {
             writeString(id)
-            writeString(name)
+            writeList(name)
             writeDouble(location!!.latitude)
             writeDouble(location!!.longitude)
             writeInt(type)
