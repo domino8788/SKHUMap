ec93ce006dbe39ece7dbc14c77cdbc2edbab323f
namjin <domino8788@naver.com>
2020-04-28 17:18:41 +0900

    #20 필드 변경사항 반영

--- app/src/main/java/com/domino/skhumap/dto/Facility.kt
+++ app/src/main/java/com/domino/skhumap/dto/Facility.kt
@@ -8,7 +8,7 @@ import com.naver.maps.map.overlay.Marker
 @IgnoreExtraProperties
 data class Facility(
     @DocumentId var id: String = "",
-    @PropertyName("name") var name: String = "",
+    @PropertyName("name") var name: List<String> = mutableListOf(),
     @PropertyName("location") var location: GeoPoint? = null,
     @PropertyName("type") var type: Int = 0,
     @PropertyName("info") var info: HashMap<String, Any>? = null
@@ -20,7 +20,7 @@ data class Facility(
     constructor(parcel: Parcel) : this() {
         parcel.run {
             id = readString()!!
-            name = readString()!!
+            readList(name, String.javaClass.classLoader)!!
             location = GeoPoint(parcel.readDouble(), parcel.readDouble())
             type = readInt()
             info = readSerializable() as HashMap<String, Any>
@@ -28,13 +28,34 @@ data class Facility(
     }
 
     enum class TYPE(val id:Int, val icon:Int){
-        DEPARTMENT(0, R.drawable.ic_apartment_black_24dp)
+        DEPARTMENT(0, R.drawable.ic_apartment_black_24dp),
+        LECTUREROOM(1, R.drawable.ic_lecture_room),
+        LIBRARY(2, R.drawable.ic_book),
+        ADMINISTRATION(3, R.drawable.ic_work),
+        PROJECTROOM(4, R.drawable.ic_apartment_black_24dp),
+        PROFESSORROOM(5, R.drawable.ic_professor_room),
+        DOOR(6, R.drawable.ic_meeting_room_24px),
+        INFO(7, 0),
+        UPSTAIRS(8, R.drawable.ic_up_stair),
+        DOWNSTAIRS(9, R.drawable.ic_down_stair),
+        ENTRANCE(10, R.drawable.ic_entrance),
+        ELEVATOR(11, R.drawable.ic_elevator),
+        TOILET_MAN(12, R.drawable.ic_toilet_man),
+        TOILET_WOMAN(13, R.drawable.ic_toilet_woman),
+        CAFE(14, R.drawable.ic_apartment_black_24dp),
+        DEPARTMENTROOM(15, R.drawable.ic_apartment_black_24dp),
+        POSTOFFICE(16, R.drawable.ic_apartment_black_24dp),
+        STATIONERY(17, R.drawable.ic_apartment_black_24dp)
+    }
+
+    fun removeMarker() {
+        marker?.map = null
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.run {
             writeString(id)
-            writeString(name)
+            writeList(name)
             writeDouble(location!!.latitude)
             writeDouble(location!!.longitude)
             writeInt(type)
