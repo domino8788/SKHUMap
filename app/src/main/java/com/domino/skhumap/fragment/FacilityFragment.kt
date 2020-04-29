d39264febe8732f430858b85c42120cc69b3d352
namjin <domino8788@naver.com>
2020-04-29 21:51:03 +0900

    #26 교내시설 Fragment 에 RecyclerView 어댑터 적용

--- app/src/main/java/com/domino/skhumap/fragment/FacilityFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/FacilityFragment.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.domino.skhumap.R
+import com.domino.skhumap.adapter.FavoritesAdapter
+import com.domino.skhumap.dto.SearchableFacility
+import kotlinx.android.synthetic.main.fragment_facility.*
 
-class FacilityFragment : Fragment() {
+class FacilityFragment : Fragment {
+
+    private constructor():super()
+
+    val searchableFacilityList:MutableList<SearchableFacility> = mutableListOf()
 
     companion object{
         private val facilityFragment = FacilityFragment()
@@ -22,5 +30,7 @@ class FacilityFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        list_facility.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        list_facility.adapter = FavoritesAdapter(searchableFacilityList)
     }
 }
