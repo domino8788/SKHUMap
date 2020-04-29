95df1f79b7ee7b5e002c5045e3b52c80e3f9e54d
namjin <domino8788@naver.com>
2020-04-29 22:13:06 +0900

    #27 페이징 기능 추가

--- app/src/main/java/com/domino/skhumap/fragment/FacilityFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/FacilityFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.domino.skhumap.R
 import com.domino.skhumap.adapter.FavoritesAdapter
 import com.domino.skhumap.dto.SearchableFacility
@@ -32,5 +33,6 @@ class FacilityFragment : Fragment {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         list_facility.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         list_facility.adapter = FavoritesAdapter(searchableFacilityList)
+        PagerSnapHelper().attachToRecyclerView(list_facility)
     }
 }
