b4b14df884ff5a00bb98afe7d8bc1cc90b5d40b7
namjin <domino8788@naver.com>
2020-04-30 13:48:14 +0900

    #28 편집 액티비티 파일 추가

---
+++ app/src/main/java/com/domino/skhumap/activity/EditFavoritesActivity.kt
@@ -0,0 +1,17 @@
+package com.domino.skhumap.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.domino.skhumap.R
+
+
+class EditFavoritesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_favorites)
+
+    }
+
+}
