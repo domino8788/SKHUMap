88eeff435714172542fd2ff321d30f687fb293fa
namjin <domino8788@naver.com>
2020-04-30 13:59:00 +0900

    #28 뒤로가기 버튼 클릭 시 즐겨찾기 편집 액티비티가 종료되게 구현

--- app/src/main/java/com/domino/skhumap/activity/EditFavoritesActivity.kt
+++ app/src/main/java/com/domino/skhumap/activity/EditFavoritesActivity.kt
@@ -21,4 +21,14 @@ class EditFavoritesActivity : AppCompatActivity() {
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
