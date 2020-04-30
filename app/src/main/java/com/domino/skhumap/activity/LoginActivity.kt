241ff2c9d3fff6975b0ead60e4b77120990554ca
namjin <domino8788@naver.com>
2020-04-30 15:52:01 +0900

    #39 툴바 추가 및 뒤로가기 버튼 기능 구현

--- app/src/main/java/com/domino/skhumap/activity/LoginActivity.kt
+++ app/src/main/java/com/domino/skhumap/activity/LoginActivity.kt
@@ -2,12 +2,26 @@ package com.domino.skhumap.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.domino.skhumap.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setSupportActionBar(login_tool_bar)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
