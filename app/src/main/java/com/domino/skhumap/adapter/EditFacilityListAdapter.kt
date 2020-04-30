e65894324a21520cb1f66252ef76683c01f255c1
namjin <domino8788@naver.com>
2020-04-30 14:00:42 +0900

    #28 즐겨찾기 편집 액티비티의 어댑터 구현

---
+++ app/src/main/java/com/domino/skhumap/adapter/EditFacilityListAdapter.kt
@@ -0,0 +1,60 @@
+package com.domino.skhumap.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.domino.skhumap.R
+import com.domino.skhumap.dto.SearchableFacility
+import kotlinx.android.synthetic.main.item_favorites_edit_list_item.view.*
+
+class EditFacilityListAdapter(private val list: MutableList<SearchableFacility>): RecyclerView.Adapter<EditFacilityListAdapter.EditFacilityViewHolder>()  {
+
+    val checkedList = mutableListOf<Int>()
+    var isSelectAll:Boolean? = null
+
+    inner class EditFacilityViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_favorites_edit_list_item, parent, false)) {
+        val check_box_select = itemView.edit_item_check_box
+        val img_icon = itemView.edit_item_img_icon
+        val txt_title = itemView.edit_item_txt_title
+        val btn_move = itemView.edit_item_btn_move
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): EditFacilityListAdapter.EditFacilityViewHolder = EditFacilityViewHolder(parent)
+
+    override fun getItemCount(): Int = if(list.size==0) 1 else list.size
+
+    override fun onBindViewHolder(holder: EditFacilityListAdapter.EditFacilityViewHolder, position: Int) {
+        if(list.size == 0 ){
+            holder.run {
+                txt_title.text = "즐겨찾기가 없습니다."
+                check_box_select.visibility = View.GONE
+                btn_move.visibility = View.GONE
+            }
+        } else {
+            holder.run {
+                txt_title.text = list[position].facility.id
+                img_icon.setImageResource(list[position].facility.resourceId)
+
+
+                check_box_select.run {
+                    isSelectAll?.let { isChecked = it }
+                    setOnCheckedChangeListener { buttonView, isChecked ->
+                        if(isChecked) {
+                            checkedList.add(position)
+                        } else {
+                            checkedList.remove(position)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int = if(list.size == 0) 0 else 1
+
+
+}
