1360b2d3947e49c37ae65be75abca96d7375d064
namjin <domino8788@naver.com>
2020-04-30 15:37:22 +0900

    #39 로그인 액티비티 추가

---
+++ app/src/main/java/com/domino/skhumap/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.domino.skhumap.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.domino.skhumap.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
