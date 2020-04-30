340cac50cfefff063f9627f0e90d3d06b86a2f70
namjin <domino8788@naver.com>
2020-04-30 16:07:01 +0900

    #39 툴바 표시 및 제목 삭제

--- app/src/main/java/com/domino/skhumap/activity/LoginActivity.kt
+++ app/src/main/java/com/domino/skhumap/activity/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.domino.skhumap.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.WindowManager
 import com.domino.skhumap.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -11,8 +12,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         setSupportActionBar(login_tool_bar)
+        supportActionBar?.run {
+            setDisplayHomeAsUpEnabled(true)
+            title = ""
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
