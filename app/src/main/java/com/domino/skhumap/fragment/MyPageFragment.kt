30dd2b30b2e79fb82e65918517eb76fa019466ee
namjin <domino8788@naver.com>
2020-04-30 15:41:39 +0900

    #36 마이페이지에서 로그인 버튼 클릭시 로그인 액티비티가 뜨게 구현

--- app/src/main/java/com/domino/skhumap/fragment/MyPageFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/MyPageFragment.kt
@@ -1,10 +1,14 @@
 package com.domino.skhumap.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.domino.skhumap.R
+import com.domino.skhumap.activity.LoginActivity
+import kotlinx.android.synthetic.main.fragment_my_page.*
 
 class MyPageFragment(): Fragment() {
 
@@ -17,11 +21,14 @@ class MyPageFragment(): Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        my_page_btn_login.setOnClickListener {
+            startActivity(Intent(context, LoginActivity::class.java))
+        }
     }
 
 }
