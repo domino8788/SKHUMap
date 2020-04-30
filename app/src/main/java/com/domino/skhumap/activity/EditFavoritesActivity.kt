00cc7af37d0cb3ce458b317c3b8f3d1db1a04cd0
namjin <domino8788@naver.com>
2020-04-30 13:52:49 +0900

    #28 툴바에 뒤로가기 추가 및 제목 삭제

--- app/src/main/java/com/domino/skhumap/activity/EditFavoritesActivity.kt
+++ app/src/main/java/com/domino/skhumap/activity/EditFavoritesActivity.kt
@@ -1,6 +1,7 @@
 package com.domino.skhumap.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 import com.domino.skhumap.R
@@ -11,6 +12,12 @@ class EditFavoritesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_favorites)
+
+        setSupportActionBar(edit_tool_bar)
+        supportActionBar?.run {
+            setDisplayHomeAsUpEnabled(true)
+            title = ""
+        }
 
     }
 
