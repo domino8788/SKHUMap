90b84f210015d9a7c5702d607c4019ac48a32d30
namjin <domino8788@naver.com>
2020-04-30 14:47:41 +0900

    #36 마이페이지 Fragment 추가

---
+++ app/src/main/java/com/domino/skhumap/fragment/MyPageFragment.kt
@@ -0,0 +1,27 @@
+package com.domino.skhumap.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MyPageFragment(): Fragment() {
+
+    companion object{
+        val instance = MyPageFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
