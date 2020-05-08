eb942512cab94cb79fb6b14652b43b0a92cd6128
namjin <domino8788@naver.com>
2020-05-08 21:12:43 +0900

    #83 Code 싱글톤 구현

---
+++ app/src/main/java/com/domino/skhumap/contract/Code.kt
@@ -0,0 +1,9 @@
+package com.domino.skhumap.contract
+
+object Code {
+    const val REQUEST_LOGIN = 10
+    const val REQUEST_EDIT_FAVORITES = 11
+    const val RESULT_REQUEST_FAVORITES_RENEWAL = 20
+    const val RESULT_REQUEST_MY_PAGE_RENEWAL = 21
+
+}
