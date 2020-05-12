0f92dc4614ad852a1b30089eb8d92898c20cad63
namjin <domino8788@naver.com>
2020-05-12 23:48:31 +0900

    #90 MyPagePreferenceFragment에서 로그아웃 클릭 시 ViewModel을 통해 로그아웃 하도록 수정

--- app/src/main/java/com/domino/skhumap/fragment/MyPagePreferenceFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/MyPagePreferenceFragment.kt
@@ -1,23 +1,25 @@
 package com.domino.skhumap.fragment
 
-import android.content.Context
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.domino.skhumap.R
+import com.domino.skhumap.model.AuthViewModel
 
 class MyPagePreferenceFragment:PreferenceFragmentCompat(){
 
+    private lateinit var authViewModel: AuthViewModel
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.my_page_preference)
+        authViewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
     }
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
         when(preference?.key){
             "logout" -> {
-                android.webkit.CookieManager.getInstance().removeAllCookie()
-                context?.getSharedPreferences("login_info", Context.MODE_PRIVATE)!!.edit().clear().commit()
-                (parentFragment as MyPageFragment).initLoginInfo()
+                authViewModel.logout()
             }
         }
         return super.onPreferenceTreeClick(preference)
