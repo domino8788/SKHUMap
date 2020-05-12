f7786a9466f5075507faf24b9bfbb17fead74bd1
namjin <domino8788@naver.com>
2020-05-12 14:35:53 +0900

    #90 SearchableFacility 에 Parcelable 상속 및 구현

--- app/src/main/java/com/domino/skhumap/dto/SearchableFacility.kt
+++ app/src/main/java/com/domino/skhumap/dto/SearchableFacility.kt
@@ -1,5 +1,34 @@
 package com.domino.skhumap.dto
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.domino.skhumap.Facility
 
-data class SearchableFacility(val department: Facility, val floorNumber:Int, val facility:Facility){}
+data class SearchableFacility(val department: Facility, val floorNumber:Int, val facility:Facility):Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readParcelable(Facility::class.java.classLoader)!!,
+        parcel.readInt(),
+        parcel.readParcelable(Facility::class.java.classLoader)!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(department, flags)
+        parcel.writeInt(floorNumber)
+        parcel.writeParcelable(facility, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<SearchableFacility> {
+        override fun createFromParcel(parcel: Parcel): SearchableFacility {
+            return SearchableFacility(parcel)
+        }
+
+        override fun newArray(size: Int): Array<SearchableFacility?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
