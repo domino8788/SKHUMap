bb8630c7927d94afff24855cb06f9f4ea9ad9068
namjin <domino8788@naver.com>
2020-05-12 23:47:45 +0900

    #90 MyPageFragment를 MVVM 패턴에 맞게 수정

--- app/src/main/java/com/domino/skhumap/fragment/MyPageFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/MyPageFragment.kt
@@ -1,19 +1,22 @@
 package com.domino.skhumap.fragment
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.domino.skhumap.R
 import com.domino.skhumap.activity.LoginActivity
 import com.domino.skhumap.contract.Code
+import com.domino.skhumap.model.AuthViewModel
 import kotlinx.android.synthetic.main.fragment_my_page.*
 
 class MyPageFragment(): Fragment() {
+
+    private lateinit var authViewModel:AuthViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,27 +27,33 @@ class MyPageFragment(): Fragment() {
     }
 
     fun initLoginInfo(){
-        if(isLogin){
-            my_page_btn_login.run {
-                visibility=View.GONE
-                setOnClickListener(null)
-            }
-            my_page_logged_in.visibility=View.VISIBLE
-            context?.getSharedPreferences("login_info", Context.MODE_PRIVATE)!!.run {
-                my_page_name.text =  getString("name", "")
-                my_page_student_number.text = getString("id", "")
-            }
-        }else {
-            my_page_btn_login.run {
-                visibility = View.VISIBLE
-                setOnClickListener { startActivityForResult(Intent(context, LoginActivity::class.java), Code.REQUEST_LOGIN) }
-            }
-            my_page_logged_in.visibility = View.INVISIBLE
+        authViewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java].apply {
+                idLiveData.observe(requireActivity(), Observer { id ->
+                    my_page_student_number.text = id
+                })
+                nameLiveData.observe(requireActivity(), Observer { name ->
+                    my_page_name.text = name
+                })
+                passwordLiveData.observe(requireActivity(), Observer { password ->
+                    if(isLogin) {
+                        my_page_btn_login.run {
+                            visibility=View.GONE
+                            setOnClickListener(null)
+                        }
+                        my_page_logged_in.visibility=View.VISIBLE
+                    }
+                    else {
+                        my_page_btn_login.run {
+                            visibility = View.VISIBLE
+                            setOnClickListener { startActivityForResult(Intent(context, LoginActivity::class.java), Code.REQUEST_LOGIN) }
+                        }
+                        my_page_logged_in.visibility = View.GONE
+                    }
+                })
+                loadLoginInfo()
         }
     }
 
-    val isLogin:Boolean
-    get() = (context?.getSharedPreferences("login_info", Context.MODE_PRIVATE)?.getString("name", "") != "")
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
