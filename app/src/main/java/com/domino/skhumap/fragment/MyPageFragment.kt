6c6a288fcbf43ec89ce8ca93bf37b31e5e081142
namjin <domino8788@naver.com>
2020-05-17 14:13:08 +0900

    #100 MyPageFragment 로그인 전 상태 관련 코드 삭제

--- app/src/main/java/com/domino/skhumap/fragment/MyPageFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/MyPageFragment.kt
@@ -1,6 +1,5 @@
 package com.domino.skhumap.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.domino.skhumap.R
-import com.domino.skhumap.activity.LoginActivity
-import com.domino.skhumap.contract.Code
 import com.domino.skhumap.model.AuthViewModel
 import kotlinx.android.synthetic.main.fragment_my_page.*
 
@@ -26,31 +23,15 @@ class MyPageFragment(): Fragment() {
         return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 
-    fun initLoginInfo(){
+    private fun initLoginInfo(){
         authViewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java].apply {
-                idLiveData.observe(requireActivity(), Observer { id ->
-                    my_page_student_number.text = id
-                })
-                nameLiveData.observe(requireActivity(), Observer { name ->
-                    my_page_name.text = name
-                })
-                passwordLiveData.observe(requireActivity(), Observer { password ->
-                    if(isLogin) {
-                        my_page_btn_login.run {
-                            visibility=View.GONE
-                            setOnClickListener(null)
-                        }
-                        my_page_logged_in.visibility=View.VISIBLE
-                    }
-                    else {
-                        my_page_btn_login.run {
-                            visibility = View.VISIBLE
-                            setOnClickListener { startActivityForResult(Intent(context, LoginActivity::class.java), Code.REQUEST_LOGIN) }
-                        }
-                        my_page_logged_in.visibility = View.GONE
-                    }
-                })
-                loadLoginInfo()
+            idLiveData.observe(requireActivity(), Observer { id ->
+                my_page_student_number.text = id
+            })
+            nameLiveData.observe(requireActivity(), Observer { name ->
+                my_page_name.text = name
+            })
+            loadLoginInfo()
         }
     }
 
