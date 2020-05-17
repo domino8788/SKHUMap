d14ff0f66d1ca817ea6ae195d4fcdc8b8de90806
namjin <domino8788@naver.com>
2020-05-17 18:56:36 +0900

    #101 기존 비밀번호를 입력받는 dialog 구현

--- app/src/main/java/com/domino/skhumap/activity/LoginActivity.kt
+++ app/src/main/java/com/domino/skhumap/activity/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.domino.skhumap.activity
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,7 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.domino.skhumap.model.AuthViewModel
+import kotlinx.android.synthetic.main.dialog_reset_password.view.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -64,6 +66,25 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
             }
+            callResetPassword.observe(this@LoginActivity, Observer { newPasswordToValid ->
+                /* 인증 재시도에 따른 view disabled */
+                login_btn.isEnabled = false
+                login_id.isEnabled = false
+                login_password.isEnabled = false
+                login_progress_bar.visibility = View.VISIBLE
+                /* 기존 비밀번호를 입력받고 인증을 시도하는 Dialog 생성 */
+                val view = View.inflate(this@LoginActivity, R.layout.dialog_reset_password, null)
+                AlertDialog.Builder(this@LoginActivity).setView(view)
+                    .setPositiveButton("reset password"){ dialog, which ->
+                        /* 인증 및 변경 시도 */
+                        resetPassword(login_id.text.toString().trim(), newPasswordToValid.first, view.dialog_password.text.toString().trim(), newPasswordToValid.second)
+                    }
+                    .setNegativeButton("cancel"){ dialog, which ->
+                        /* 취소버튼 클릭 */
+                        toastLiveData.postValue("인증을 취소했습니다.")
+                        dialog.cancel()
+                    }.create().show()
+            })
             if(isLogin) {
                 app.getSharedPreferences("login_info", Context.MODE_PRIVATE).run {
                     login_id.setText(getString("id", ""))
