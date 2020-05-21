5fedf3f13b4692b3f194ab3c3d5fb73ecb8e3e94
namjin <domino8788@naver.com>
2020-05-22 00:59:12 +0900

    #107 함수명 수정 및 컬렉션과 문서에 대한 참조 추가

--- app/src/main/java/com/domino/skhumap/db/FirestoreHelper.kt
+++ app/src/main/java/com/domino/skhumap/db/FirestoreHelper.kt
@@ -3,46 +3,50 @@ package com.domino.skhumap.db
 import android.content.Context
 import com.domino.skhumap.Facility
 import com.google.firebase.FirebaseApp
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.DocumentChange
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.*
 
 object FirestoreHelper {
     private var realTimeUpdateListener: ListenerRegistration? = null
-
-    fun realTimeUpdate(facilities:MutableList<Facility> ,queryTarget: CollectionReference ,callback: (() -> Unit)?) {
-        realTimeUpdateListener?.remove()
-        realTimeUpdateListener = queryTarget.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-            for(change in querySnapshot?.documentChanges!!){
-                change.run {
-                    when (type) {
-                        DocumentChange.Type.ADDED -> {
-                            facilities.add(newIndex, document.toObject(Facility::class.java))
-                        }
-                        DocumentChange.Type.MODIFIED -> {
-                            facilities[oldIndex].marker?.map = null
-                            facilities[newIndex] = document.toObject(Facility::class.java)
-                        }
-                        DocumentChange.Type.REMOVED -> {
-                            facilities[oldIndex].removeMarker()
-                            facilities.removeAt(oldIndex)
-                        }
-                    }
-                }
-            }
-            (callback)?.let { func -> func() }
-        }
-    }
-
-    private val db by lazy { FirebaseFirestore.getInstance() }
+    val db by lazy { FirebaseFirestore.getInstance().apply {
+        firestoreSettings = FirebaseFirestoreSettings.Builder().setCacheSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED).build()
+    } }
     private const val COLLECTION_FACILITIES = "facilities"
 
+    lateinit var userReference: DocumentReference
     val campusReference by lazy { db.collection(COLLECTION_FACILITIES) }
-    fun departmentReference(departmentId:String, floor:Int): CollectionReference = campusReference.document(departmentId).collection(floor.toString())
+    val favoritesReference by lazy { userReference.collection("favorites") }
+    fun departmentReference(departmentId: String, floor: Int): CollectionReference =
+        campusReference.document(departmentId).collection(floor.toString())
 
-    fun init(context:Context){
+    fun init(context: Context) {
         FirebaseApp.initializeApp(context)
     }
 
+    fun naverMapRealTimeUpdate(
+        facilities: MutableList<Facility>,
+        queryTarget: CollectionReference,
+        callback: (() -> Unit)?
+    ) {
+        realTimeUpdateListener?.remove()
+        realTimeUpdateListener = queryTarget.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                for (change in querySnapshot?.documentChanges!!) {
+                    change.run {
+                        when (type) {
+                            DocumentChange.Type.ADDED -> {
+                                facilities.add(newIndex, document.toObject(Facility::class.java))
+                            }
+                            DocumentChange.Type.MODIFIED -> {
+                                facilities[oldIndex].marker?.map = null
+                                facilities[newIndex] = document.toObject(Facility::class.java)
+                            }
+                            DocumentChange.Type.REMOVED -> {
+                                facilities[oldIndex].removeMarker()
+                                facilities.removeAt(oldIndex)
+                            }
+                        }
+                    }
+                }
+                (callback)?.let { func -> func() }
+            }
+    }
 }
