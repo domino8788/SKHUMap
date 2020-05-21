0017ad0357f4563a28ae99195db1400f15f9a9a3
namjin <domino8788@naver.com>
2020-05-22 00:38:27 +0900

    #110 Favorites 객체를 SearchableFacility로 변환하는 기능 구현

--- app/src/main/java/com/domino/skhumap/dto/Favorites.kt
+++ app/src/main/java/com/domino/skhumap/dto/Favorites.kt
@@ -1,5 +1,6 @@
 package com.domino.skhumap.dto
 
+import com.domino.skhumap.Facility
 import com.domino.skhumap.db.FirestoreHelper.favoritesReference
 import com.google.firebase.firestore.*
 import java.io.Serializable
@@ -16,4 +17,21 @@ data class Favorites(@PropertyName("index") var index:Int = 0,
         "${department?.id?:"null"}_${floorNumber}_${facility!!.id}"
     )
 
+    /* DB에서 조회한 Favorites 객체를 검색가능한 시설객체로 변환 */
+    fun toSearchableFacility(result:HashMap<Int, SearchableFacility>, delete:HashMap<Int, DocumentReference> ,callback: (()->Unit)?) {
+        department?.let { department ->
+            department.get().addOnCompleteListener { departmentTask ->
+                facility!!.get().addOnCompleteListener { facilityTask ->
+                    facilityTask.result?.toObject(Facility::class.java)?.let {
+                        result[index] = SearchableFacility(
+                            departmentTask.result?.toObject(Facility::class.java),
+                            floorNumber,
+                            it
+                        )
+                    } ?: let { delete[index] = toReference() }
+                    (callback)?.let { func -> func() }
+                }
+            }
+        }
+    }
 }
