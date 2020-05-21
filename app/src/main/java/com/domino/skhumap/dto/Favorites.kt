cf1356756313882e15fd84132e0eb8a186a04bbf
namjin <domino8788@naver.com>
2020-05-22 00:37:57 +0900

    #110 Favorites 객체를 참조로 변환하는 기능 구현

--- app/src/main/java/com/domino/skhumap/dto/Favorites.kt
+++ app/src/main/java/com/domino/skhumap/dto/Favorites.kt
@@ -1,5 +1,6 @@
 package com.domino.skhumap.dto
 
+import com.domino.skhumap.db.FirestoreHelper.favoritesReference
 import com.google.firebase.firestore.*
 import java.io.Serializable
 
@@ -9,5 +10,10 @@ data class Favorites(@PropertyName("index") var index:Int = 0,
                      @PropertyName("floorNumber")var floorNumber:Int = 0,
                      @PropertyName("facility") var facility:DocumentReference? = null):Serializable {
 
+
+    /* Favorites 객체의 정보로 해당 정보가 담겨있는 DocumentReference 를 반환한다. */
+    fun toReference() = favoritesReference.document(
+        "${department?.id?:"null"}_${floorNumber}_${facility!!.id}"
+    )
 
 }
