6d473ca1ce6333f482b059aeced463e7481da9b6
namjin <domino8788@naver.com>
2020-05-22 00:39:34 +0900

    #110 SearchableFacility를 문서 참조로 변환하는 기능 구현

--- app/src/main/java/com/domino/skhumap/dto/SearchableFacility.kt
+++ app/src/main/java/com/domino/skhumap/dto/SearchableFacility.kt
@@ -3,14 +3,19 @@ package com.domino.skhumap.dto
 import android.os.Parcel
 import android.os.Parcelable
 import com.domino.skhumap.Facility
+import com.domino.skhumap.db.FirestoreHelper
 
-data class SearchableFacility(val department: Facility, val floorNumber:Int, val facility:Facility):Parcelable {
+data class SearchableFacility(val department: Facility?, val floorNumber:Int=0, val facility:Facility):Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readParcelable(Facility::class.java.classLoader)!!,
         parcel.readInt(),
         parcel.readParcelable(Facility::class.java.classLoader)!!
-    ) {
-    }
+    )
+
+    /* SearchableFacility 객체의 정보로 해당 정보가 담겨있는 DocumentReference 를 반환한다. */
+    fun toReference() = FirestoreHelper.favoritesReference.document(
+        "${department?.id?:"null"}_${floorNumber}_${facility.id}"
+    )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeParcelable(department, flags)
