5a0031bb22ecf1c798df9602e6b7c473169c36c7
namjin <domino8788@naver.com>
2020-05-22 10:31:42 +0900

    #107 즐겨찾기 추가 및 저장 기능 구현

--- app/src/main/java/com/domino/skhumap/model/FavoritesViewModel.kt
+++ app/src/main/java/com/domino/skhumap/model/FavoritesViewModel.kt
@@ -9,6 +9,19 @@ class FavoritesViewModel : ViewModel() {
 
     val favoritesLiveData: MutableLiveData<ArrayList<SearchableFacility>> = MutableLiveData<ArrayList<SearchableFacility>>(favorites)
 
+    /* 즐겨찾기 삽입 */
+    fun insert(searchableFavorites: SearchableFacility) {
+        if(!favorites.contains(searchableFavorites)){
+            favorites.add(searchableFavorites)
+            favoritesLiveData.postValue(favorites)
+            searchableFavorites.toReference().set(
+                searchableFavorites.toFavorites(favorites.size-1)
+            ).addOnFailureListener {
+                favorites.remove(searchableFavorites)
+                favoritesLiveData.postValue(favorites)
+            }
+        }
+    }
 
 
 }
