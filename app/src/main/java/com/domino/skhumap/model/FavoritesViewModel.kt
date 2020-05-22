4a2cba11711eaec839136764d0f547230dc4c562
namjin <domino8788@naver.com>
2020-05-22 10:32:59 +0900

    #108 즐겨찾기 조회 기능 구현

--- app/src/main/java/com/domino/skhumap/model/FavoritesViewModel.kt
+++ app/src/main/java/com/domino/skhumap/model/FavoritesViewModel.kt
@@ -2,10 +2,16 @@ package com.domino.skhumap.model
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.domino.skhumap.db.FirestoreHelper.db
+import com.domino.skhumap.db.FirestoreHelper.favoritesReference
+import com.domino.skhumap.dto.Favorites
 import com.domino.skhumap.dto.SearchableFacility
+import com.google.firebase.firestore.DocumentReference
 
 class FavoritesViewModel : ViewModel() {
     private val favorites: ArrayList<SearchableFacility> = ArrayList()
+    private val result: HashMap<Int, SearchableFacility> = HashMap()
+    private val delete: HashMap<Int, DocumentReference> = HashMap()
 
     val favoritesLiveData: MutableLiveData<ArrayList<SearchableFacility>> = MutableLiveData<ArrayList<SearchableFacility>>(favorites)
 
@@ -23,5 +29,29 @@ class FavoritesViewModel : ViewModel() {
         }
     }
 
-
+    fun query() {
+        favoritesReference.orderBy("index").get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                task.result!!.toObjects(Favorites::class.java).forEach {
+                    it.toSearchableFacility(result, delete) {
+                        if ((result.size + delete.size) == task.result!!.size()) {
+                            favorites.clear()
+                            favorites.addAll(result.values)
+                            favoritesLiveData.postValue(favorites)
+                            if (delete.size != 0) {
+                                db.runBatch { batch ->
+                                    delete.values.forEach { deleteTarget ->
+                                        batch.delete(deleteTarget)
+                                    }
+                                    favorites.forEachIndexed { index, updateTarget ->
+                                        batch.update(updateTarget.toReference(), mapOf("index" to index))
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
