682ff488e316be44b872947956636b7d43df36d7
namjin <domino8788@naver.com>
2020-05-23 20:28:42 +0900

    #118 모든 시설 데이터를 시설 검색 데이터로 변환 및 저장하는 기능 구현

--- server/functions/index.js
+++ server/functions/index.js
@@ -38,3 +38,41 @@ const generateKeywords = (id, names) => {
     }
     return keywordsArray
 }
+
+exports.allKeywordsGenerate =  functions.https.onRequest(async (req, res) => {
+    let log = "execute onceGenerate\n"
+
+    const departments = await db.collection('facilities').listDocuments()
+    const departmentsPromise = []
+    departments.forEach((department) => {
+        departmentsPromise.push(department.listCollections())
+    })
+
+    const allFloors = await Promise.all(departmentsPromise)
+    const floorsPromise = []
+    allFloors.forEach(floors => {
+        floors.forEach(floor => {
+            floorsPromise.push(floor.get());
+        })
+    })
+
+    const allFacilities = await Promise.all(floorsPromise);
+    allFacilities.forEach((facilities) => {
+        facilities.forEach((facility) => {
+            const floor = facility.ref.parent
+            const department = floor.parent
+            log += `${department.id}_${floor.id}_${facility.id}\n`
+            
+            let data= facility.data()
+            let id = facility.id
+            let names = data["name"]
+    
+            db.doc(`search/${department.id}_${floor.id}_${facility.id}`).set({
+                keyword: [id].concat(names),
+                keywords : generateKeywords(id, names)
+            })
+        })
+    })
+    log+="complete onceGenerate\n"
+    res.send(302, `<pre>${log}</pre>`)
+})
