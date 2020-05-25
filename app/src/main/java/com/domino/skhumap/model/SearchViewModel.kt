e49862eaa84a51f23f8b7e4070f2e9b3b23bd65b
namjin <domino8788@naver.com>
2020-05-25 23:06:45 +0900

    #113 검색어를 매개변수로 시설 조회기능 구현

--- app/src/main/java/com/domino/skhumap/model/SearchViewModel.kt
+++ app/src/main/java/com/domino/skhumap/model/SearchViewModel.kt
@@ -2,10 +2,23 @@ package com.domino.skhumap.model
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.domino.skhumap.db.FirestoreHelper
 import com.domino.skhumap.dto.Search
 
 class SearchViewModel : ViewModel() {
 
     val searchListMapLivdeData: MutableLiveData<List<Search>> by lazy { MutableLiveData<List<Search>>() }
 
+    fun queryText(text: String) {
+        FirestoreHelper
+            .searchReference
+            .whereArrayContains("keywords", text.trim().toUpperCase())
+            .limit(6)
+            .get()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    searchListMapLivdeData.postValue(it.result?.toObjects(Search::class.java))
+                }
+            }
+    }
 }
