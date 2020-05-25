dbd508cfc1af5d2fd3bda9e547b37ab2a8d5978c
namjin <domino8788@naver.com>
2020-05-25 23:03:03 +0900

    #113 Search 클래스에 SearchSuggestion 상속 및 구현

--- app/src/main/java/com/domino/skhumap/dto/Search.kt
+++ app/src/main/java/com/domino/skhumap/dto/Search.kt
@@ -1,6 +1,37 @@
 package com.domino.skhumap.dto
 
+import android.os.Parcel
+import android.os.Parcelable
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion
 import com.google.firebase.firestore.*
 
 @IgnoreExtraProperties
-data class Search(@DocumentId var id: String="", @PropertyName("keyword") var keyword:List<String>? = null)
+data class Search(@DocumentId var id: String="", @PropertyName("keyword") var keyword:List<String>? = null):SearchSuggestion {
+
+    constructor(parcel: Parcel) : this() {
+        id = parcel.readString()!!
+        parcel.readList(keyword, String::class.java.classLoader)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeList(keyword)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun getBody(): String = "${keyword!!.joinToString(" ","","")}"
+
+    companion object CREATOR : Parcelable.Creator<Search> {
+        override fun createFromParcel(parcel: Parcel): Search {
+            return Search(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Search?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
