8c27d244fbc5f2c49e4847854d164312e3f9df0a
namjin <domino8788@naver.com>
2020-05-26 00:34:19 +0900

    #123 MainViewModel 구현

---
+++ app/src/main/java/com/domino/skhumap/model/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.domino.skhumap.model
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.domino.skhumap.view.MultipleLevelBottomSheetView
+
+class MainViewModel:ViewModel() {
+    val bottomSheetStateLiveData: MutableLiveData<MultipleLevelBottomSheetView.State> by lazy { MutableLiveData<MultipleLevelBottomSheetView.State>() }
+}
