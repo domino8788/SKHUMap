a04b0cde4065fef6be1a9e3ead324d3f2be2d9dd
namjin <domino8788@naver.com>
2020-05-28 14:52:17 +0900

    #129 Lecture VO 클래스 구현

---
+++ app/src/main/java/com/domino/skhumap/vo/Lecture.kt
@@ -0,0 +1,55 @@
+package com.domino.skhumap.vo
+
+import com.google.gson.annotations.SerializedName
+
+data class Lecture (
+    @SerializedName("Yy")
+    val yy: String,
+    @SerializedName("Haggi")
+    val haggi: String,
+
+    @SerializedName("GwamogCd")
+    val gwamogCd: String,
+
+    @SerializedName("Bunban")
+    val bunban: String,
+
+    @SerializedName("GwamogKorNm")
+    val gwamogKorNm: String,
+
+    @SerializedName("YoilNm")
+    val yoilNm: String,
+
+    @SerializedName("FrTm")
+    val frTm: String,
+
+    @SerializedName("ToTm")
+    val toTm: String,
+
+    @SerializedName("AdjustFrTm")
+    val adjustFrTm: String,
+
+    @SerializedName("AdjustToTm")
+    val adjustToTm: String,
+
+    @SerializedName("Times")
+    val times: String,
+
+    @SerializedName("Bigo")
+    val bigo: String,
+
+    @SerializedName("Bigo2")
+    val bigo2: String,
+
+    @SerializedName("GyosuSabeon")
+    val gyosuSabeon: String,
+
+    @SerializedName("GyosuNm")
+    val gyosuNm: String,
+
+    @SerializedName("HosilCd")
+    val hosilCd: String,
+
+    @SerializedName("RoomName")
+    val roomName: String
+)
