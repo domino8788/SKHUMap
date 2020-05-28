e3c4c56bd563cf4ecdfdd4250bebec251e7c9e3a
namjin <domino8788@naver.com>
2020-05-28 14:49:34 +0900

    #129 RetrofitHelper 구현

---
+++ app/src/main/java/com/domino/skhumap/db/RetrofitHelper.kt
@@ -0,0 +1,41 @@
+package com.domino.skhumap.db
+
+import com.domino.skhumap.vo.Haggi
+import com.domino.skhumap.vo.StudentSchedule
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+object RetrofitHelper {
+    private val gson = GsonBuilder().setLenient().create()
+    private val httpClient = OkHttpClient.Builder().apply {
+        interceptors().add(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+    }.build()
+
+    val studentScheduleRetrofit: Retrofit
+    get() = Retrofit.Builder()
+        .baseUrl("http://sam.skhu.ac.kr")
+        .client(httpClient)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+}
+
+interface RetrofitService {
+    @Headers(
+        "content-type: application/json;charset=UTF-8",
+        "X-Requested-With: XMLHttpRequest"
+    )
+    @POST("/SSE/SSEAD/SSEAD03_GetList")
+    fun getStudentScheduleList(
+        @Body haggi: Haggi,
+        @Header("RequestVerificationToken") token: String,
+        @Header("Cookie") cookie: String
+    ): Call<StudentSchedule>
+}
