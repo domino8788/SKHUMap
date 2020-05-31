676d6a6fa11aa9cfe54a801c784b033ca982336c
namjin <domino8788@naver.com>
2020-05-31 22:51:16 +0900

    #135 #104 Schedule 에 변경사항 반영

--- app/src/main/java/com/domino/skhumap/dto/Schedule.kt
+++ app/src/main/java/com/domino/skhumap/dto/Schedule.kt
@@ -1,16 +1,20 @@
 package com.domino.skhumap.dto
 
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
 
 data class Schedule(
-    @DocumentId val id:String,
     val type:Long=0,
     var name:String="",
-    var info:HashMap<String, String>? = null,
+    var info:String? = null,
     var yoil:MutableList<String>? = null,
+    var startDate:Timestamp? = null,
+    var endDate:Timestamp? = null,
     var everyWeek:Boolean = false,
     var adjustFrTm:String = "",
     var adjustToTm:String = "",
     var frTm:String = "",
     var toTm:String = ""
-    )
+){
+    @DocumentId val id:String = ""
+}
