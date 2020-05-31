21210953b9f9a516d357b502d1b1458930248d71
namjin <domino8788@naver.com>
2020-05-31 23:56:56 +0900

    #135 MainViewModel에 데이터 요청 함수 구현

--- app/src/main/java/com/domino/skhumap/model/MainViewModel.kt
+++ app/src/main/java/com/domino/skhumap/model/MainViewModel.kt
@@ -1,13 +1,20 @@
 package com.domino.skhumap.model
 
+import android.webkit.WebViewClient
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.domino.skhumap.view.MultipleLevelBottomSheetView
 
 class MainViewModel:ViewModel() {
     val bottomSheetStateLiveData: MutableLiveData<MultipleLevelBottomSheetView.State> by lazy { MutableLiveData<MultipleLevelBottomSheetView.State>() }
+    val webClientLiveData: MutableLiveData<WebViewClient> by lazy { MutableLiveData<WebViewClient>() }
+    val toastLiveData: MutableLiveData<String> by lazy { MutableLiveData<String>() }
 
     fun setBottomSheetState(state:MultipleLevelBottomSheetView.State){
         bottomSheetStateLiveData.postValue(state)
+    }
+
+    fun requestHttp(webClient:WebViewClient){
+        webClientLiveData.postValue(webClient)
     }
 }
