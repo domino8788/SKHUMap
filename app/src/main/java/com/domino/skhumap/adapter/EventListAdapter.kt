73f0c93ae39684df1a0d63e0d280432942abe351
namjin <domino8788@naver.com>
2020-05-31 22:59:11 +0900

    #135 Schedule 클래스의 구성에 맞게 EventListAdapter 를 수정

--- app/src/main/java/com/domino/skhumap/adapter/EventListAdapter.kt
+++ app/src/main/java/com/domino/skhumap/adapter/EventListAdapter.kt
@@ -4,14 +4,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.domino.skhumap.R
-import com.domino.skhumap.dto.Event
+import com.domino.skhumap.dto.Schedule
 import com.domino.skhumap.utils.inflate
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_event_list.view.*
 
-class EventListAdapter(val onClick: (Event) -> Unit) : RecyclerView.Adapter<EventListAdapter.EventViewHolder>() {
+class EventListAdapter(val onClick: (Schedule) -> Unit) : RecyclerView.Adapter<EventListAdapter.EventViewHolder>() {
 
-    val events = mutableListOf<Event>()
+    val events = mutableListOf<Schedule>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
         return EventViewHolder(parent.inflate(R.layout.item_event_list))
@@ -32,8 +32,12 @@ class EventListAdapter(val onClick: (Event) -> Unit) : RecyclerView.Adapter<Even
             }
         }
 
-        fun bind(event: Event) {
-            containerView.txt_event.text = event.text
+        fun bind(event: Schedule) {
+            containerView.run {
+                txt_event_title.text = event.name
+                txt_event_info.text = event.info
+                txt_event_time.text = "${event.frTm} ~ ${event.toTm}"
+            }
         }
     }
 
