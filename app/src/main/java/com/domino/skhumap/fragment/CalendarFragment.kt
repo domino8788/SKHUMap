f42795d673d4c224bfb6e1dc6fc4f58fbeaf6652
namjin <domino8788@naver.com>
2020-06-01 15:07:13 +0900

    #132 일정 목록 날짜 표시 양식 수정

--- app/src/main/java/com/domino/skhumap/fragment/CalendarFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/CalendarFragment.kt
@@ -228,7 +228,7 @@ class CalendarFragment : Fragment() {
 
     private var selectedDate: LocalDate? = null
     private val today by lazy { LocalDate.now() }
-    private val selectionFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
+    private val selectionFormatter = DateTimeFormatter.ofPattern("yyyy년 MMMM d일")
     private val monthTitleFormatter = DateTimeFormatter.ofPattern("MMMM")
     private var monthToWeek:Boolean = true
 
