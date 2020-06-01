084b93bf8fe7492aae5c19f4ab2f2d4aee404e84
namjin <domino8788@naver.com>
2020-06-01 23:30:57 +0900

    #128 type 유형 변경

--- app/src/main/java/com/domino/skhumap/dto/Schedule.kt
+++ app/src/main/java/com/domino/skhumap/dto/Schedule.kt
@@ -2,6 +2,7 @@ package com.domino.skhumap.dto
 
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
+import java.io.Serializable
 
 data class Schedule(
     val type:Long=0,
@@ -15,10 +16,11 @@ data class Schedule(
     var adjustToTm:String = "",
     var frTm:String = "",
     var toTm:String = ""
-){
+):Serializable{
     companion object{
         const val TYPE_PERSONAL:Long = 0L
         const val TYPE_EDIT_STUDENT_SCHEDULE:Long = 1L
+        const val TYPE_STUDENT_SCHEDULE:Long = 2L
     }
-    @DocumentId val id:String = ""
+    @DocumentId var id:String = ""
 }
