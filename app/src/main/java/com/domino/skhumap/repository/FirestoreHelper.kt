0a9e4ab13430e78e630f1df23f063da4921b5b35
namjin <domino8788@naver.com>
2020-06-04 00:07:25 +0900

    #143 FirestoreHelper에서 기존 쿼리 로직 삭제

--- app/src/main/java/com/domino/skhumap/repository/FirestoreHelper.kt
+++ app/src/main/java/com/domino/skhumap/repository/FirestoreHelper.kt
@@ -1,12 +1,10 @@
 package com.domino.skhumap.repository
 
 import android.content.Context
-import com.domino.skhumap.Facility
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.*
 
 object FirestoreHelper {
-    private var realTimeUpdateListener: ListenerRegistration? = null
     val db by lazy { FirebaseFirestore.getInstance().apply {
         firestoreSettings = FirebaseFirestoreSettings.Builder().setCacheSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED).build()
     } }
@@ -23,33 +21,5 @@ object FirestoreHelper {
 
     fun init(context: Context) {
         FirebaseApp.initializeApp(context)
-    }
-
-    fun naverMapRealTimeUpdate(
-        facilities: MutableList<Facility>,
-        queryTarget: CollectionReference,
-        callback: (() -> Unit)?
-    ) {
-        realTimeUpdateListener?.remove()
-        realTimeUpdateListener = queryTarget.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                for (change in querySnapshot?.documentChanges!!) {
-                    change.run {
-                        when (type) {
-                            DocumentChange.Type.ADDED -> {
-                                facilities.add(newIndex, document.toObject(Facility::class.java))
-                            }
-                            DocumentChange.Type.MODIFIED -> {
-                                facilities[oldIndex].marker?.map = null
-                                facilities[newIndex] = document.toObject(Facility::class.java)
-                            }
-                            DocumentChange.Type.REMOVED -> {
-                                facilities[oldIndex].removeMarker()
-                                facilities.removeAt(oldIndex)
-                            }
-                        }
-                    }
-                }
-                (callback)?.let { func -> func() }
-            }
     }
 }
