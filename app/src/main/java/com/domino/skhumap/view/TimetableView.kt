63673f2fbaf5534f85528d7896f489e3e4ad6756
namjin <domino8788@naver.com>
2020-06-15 16:19:03 +0900

    #155 뷰에 속성값을 적용하는 기능 구현

--- app/src/main/java/com/domino/skhumap/view/TimetableView.kt
+++ app/src/main/java/com/domino/skhumap/view/TimetableView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.res.Resources
 import android.util.AttributeSet
 import android.widget.*
+import com.domino.skhumap.R
 import com.domino.skhumap.dto.Sticker
 import java.util.*
 
@@ -25,6 +26,40 @@ class TimetableView(@get:JvmName("getContext_")val context: Context, attrs: Attr
     var tableBox: TableLayout? = null
     var stickers: HashMap<Int, Sticker> = HashMap<Int, Sticker>()
     private var stickerCount = -1
+
+    private fun getAttrs(attrs: AttributeSet?) {
+        val a = context.obtainStyledAttributes(attrs, R.styleable.TimetableView)
+        rowCount = a.getInt(
+            R.styleable.TimetableView_row_count,
+            DEFAULT_ROW_COUNT
+        ) - 1
+        columnCount = a.getInt(
+            R.styleable.TimetableView_column_count,
+            DEFAULT_COLUMN_COUNT
+        )
+        cellHeight = a.getDimensionPixelSize(
+            R.styleable.TimetableView_cell_height,
+            dp2Px(DEFAULT_CELL_HEIGHT_DP)
+        )
+        headerCellHeight = a.getDimensionPixelSize(
+            R.styleable.TimetableView_cell_height,
+            dp2Px(DEFAULT_HEADER_CELL_HEIGHT_DP)
+        )
+        sideCellWidth = a.getDimensionPixelSize(
+            R.styleable.TimetableView_side_cell_width,
+            dp2Px(DEFAULT_SIDE_CELL_WIDTH_DP)
+        )
+        val titlesId = a.getResourceId(R.styleable.TimetableView_header_title, R.array.header_title)
+        headerTitle = a.resources.getStringArray(titlesId)
+        val colorsId =
+            a.getResourceId(R.styleable.TimetableView_sticker_colors, R.array.default_sticker_color)
+        stickerColors = a.resources.getStringArray(colorsId)
+        startTime = a.getInt(
+            R.styleable.TimetableView_start_time,
+            DEFAULT_START_TIME
+        )
+        a.recycle()
+    }
 
     companion object {
         private const val DEFAULT_ROW_COUNT = 17
