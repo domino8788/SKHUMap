d47cb801ce18660fcd72d46a0107458de8ef82ef
namjin <domino8788@naver.com>
2020-06-15 16:18:13 +0900

    #155 TimetableView 속성값 프로퍼티 추가

--- app/src/main/java/com/domino/skhumap/view/TimetableView.kt
+++ app/src/main/java/com/domino/skhumap/view/TimetableView.kt
@@ -4,11 +4,28 @@ import android.content.Context
 import android.content.res.Resources
 import android.util.AttributeSet
 import android.widget.*
+import com.domino.skhumap.dto.Sticker
+import java.util.*
 
 
 class TimetableView(@get:JvmName("getContext_")val context: Context, attrs: AttributeSet?, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
     constructor(context: Context):this(context, null)
     constructor(context:Context, attrs:AttributeSet?):this(context, attrs, 0)
+
+    private var rowCount = 0
+    private var columnCount = 0
+    private var cellHeight = 0
+    private var headerCellHeight = 0
+    private var sideCellWidth = 0
+    private lateinit var headerTitle: Array<String>
+    private lateinit var stickerColors: Array<String>
+    private var startTime = 0
+    private var stickerBox: RelativeLayout? = null
+    var tableHeader: TableLayout? = null
+    var tableBox: TableLayout? = null
+    var stickers: HashMap<Int, Sticker> = HashMap<Int, Sticker>()
+    private var stickerCount = -1
+
     companion object {
         private const val DEFAULT_ROW_COUNT = 17
         private const val DEFAULT_COLUMN_COUNT = 6
