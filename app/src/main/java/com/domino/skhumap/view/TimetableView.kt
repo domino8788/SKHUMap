6bd5197864d3eb514dd8eb81e04d0a032614fa71
namjin <domino8788@naver.com>
2020-06-15 11:51:52 +0900

    #155 속성 값들의 기본 값 정의

--- app/src/main/java/com/domino/skhumap/view/TimetableView.kt
+++ app/src/main/java/com/domino/skhumap/view/TimetableView.kt
@@ -1,10 +1,24 @@
 package com.domino.skhumap.view
 
 import android.content.Context
+import android.content.res.Resources
 import android.util.AttributeSet
 import android.widget.*
+
 
 class TimetableView(@get:JvmName("getContext_")val context: Context, attrs: AttributeSet?, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
     constructor(context: Context):this(context, null)
     constructor(context:Context, attrs:AttributeSet?):this(context, attrs, 0)
+    companion object {
+        private const val DEFAULT_ROW_COUNT = 17
+        private const val DEFAULT_COLUMN_COUNT = 6
+        private const val DEFAULT_CELL_HEIGHT_DP = 150
+        private const val DEFAULT_HEADER_CELL_HEIGHT_DP = 50
+        private const val DEFAULT_SIDE_CELL_WIDTH_DP = 30
+        private const val DEFAULT_START_TIME = 9
+        private const val DEFAULT_SIDE_HEADER_FONT_SIZE_DP = 13
+        private const val DEFAULT_HEADER_FONT_SIZE_DP = 15
+        private const val DEFAULT_STICKER_FONT_SIZE_DP = 13
+        private fun dp2Px(dp: Int): Int = (dp * Resources.getSystem().displayMetrics.density).toInt()
+    }
 }
