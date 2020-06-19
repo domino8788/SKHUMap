e3b9749f4bad66578b2289f3f7e9a6604d4984e3
namjin <domino8788@naver.com>
2020-06-19 16:07:20 +0900

    #158 시설의 시간표 데이터를 표시하는 기능 구현

--- app/src/main/java/com/domino/skhumap/fragment/FacilityInfoFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/FacilityInfoFragment.kt
@@ -5,10 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.domino.skhumap.R
 import com.domino.skhumap.dto.SearchableFacility
+import com.domino.skhumap.model.MainViewModel
+import com.domino.skhumap.model.MapViewModel
+import com.domino.skhumap.view.MultipleLevelBottomSheetView
+import kotlinx.android.synthetic.main.fragment_facility_info.view.*
 
 class FacilityInfoFragment(private val facility: SearchableFacility) : Fragment() {
+    private lateinit var mapViewModel: MapViewModel
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +26,16 @@ class FacilityInfoFragment(private val facility: SearchableFacility) : Fragment(
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+        mapViewModel = ViewModelProvider(this)[MapViewModel::class.java].apply {
+            selectedFacilityInfoLiveData.observe(requireActivity(), Observer { lectureList ->
+                mainViewModel.setBottomSheetState(MultipleLevelBottomSheetView.State.HIDDEN)
+                view.fragment_facility_info_timetable.load(lectureList)
+            })
+            selectedFacilityLiveData.observe(requireActivity(), Observer { searchableFacility ->
+                mainViewModel.requestHttp(getSelectedFacilityInfo(searchableFacility.facility.id))
+            })
+            selectedFacilityLiveData.postValue(facility)
+        }
     }
 }
