40337a123814ddc4aff781c1a76ebeae1d292c1c
namjin <domino8788@naver.com>
2020-06-26 11:54:39 +0900

    #152 텍스트에 애니메이션 적용

--- app/src/main/java/com/domino/skhumap/activity/SplashActivity.kt
+++ app/src/main/java/com/domino/skhumap/activity/SplashActivity.kt
@@ -1,13 +1,17 @@
 package com.domino.skhumap.activity
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.domino.skhumap.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val txtAnim = AnimationUtils.loadAnimation(this, R.anim.appear)
+        splash_txt.startAnimation(txtAnim)
     }
 }
