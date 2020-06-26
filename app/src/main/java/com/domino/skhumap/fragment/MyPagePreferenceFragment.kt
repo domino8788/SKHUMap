68e777438351a08a0b692aa04774262a5b155d4e
namjin <domino8788@naver.com>
2020-06-26 17:40:55 +0900

    #162 앱 정보 버튼 클릭 시 앱 정보 프래그먼트가 뜨게 구현

--- app/src/main/java/com/domino/skhumap/fragment/MyPagePreferenceFragment.kt
+++ app/src/main/java/com/domino/skhumap/fragment/MyPagePreferenceFragment.kt
@@ -8,14 +8,18 @@ import androidx.preference.PreferenceFragmentCompat
 import com.domino.skhumap.R
 import com.domino.skhumap.activity.LoginActivity
 import com.domino.skhumap.model.AuthViewModel
+import com.domino.skhumap.model.MainViewModel
+import com.domino.skhumap.view.MultipleLevelBottomSheetView
 
 class MyPagePreferenceFragment:PreferenceFragmentCompat(){
 
     private lateinit var authViewModel: AuthViewModel
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.my_page_preference)
         authViewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
     }
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
@@ -26,6 +30,22 @@ class MyPagePreferenceFragment:PreferenceFragmentCompat(){
                     flags = Intent.FLAG_ACTIVITY_NEW_DOCUMENT
                 })
                 requireActivity().finish()
+            }
+            "info" -> {
+                val fm = requireActivity().supportFragmentManager
+                if(fm.findFragmentByTag(AppInfoFragment.TAG) == null) {
+                    val fragment = AppInfoFragment()
+                    mainViewModel.setBottomSheetState(MultipleLevelBottomSheetView.State.HIDDEN)
+                    fm.beginTransaction()
+                        .setCustomAnimations(
+                            R.anim.slide_in_up,
+                            R.anim.fade_out,
+                            R.anim.fade_in,
+                            R.anim.slide_out_down)
+                        .add(R.id.main_layout, fragment, AppInfoFragment.TAG)
+                        .addToBackStack(null)
+                        .commit()
+                }
             }
         }
         return super.onPreferenceTreeClick(preference)
