d56369d9ce2775dbb85739ce56c3dc5ff075201f
namjin <domino8788@naver.com>
2020-06-26 11:54:18 +0900

    #152 로딩 화면 액티비티 추가

---
+++ app/src/main/java/com/domino/skhumap/activity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.domino.skhumap.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.domino.skhumap.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
