c763d54d4992328f4b7785f5d4672a76a0df2335
namjin <domino8788@naver.com>
2020-06-26 11:55:04 +0900

    #152 애니메이션이 끝나고 LoginActivity 를 실행하도록 구현

--- app/src/main/java/com/domino/skhumap/activity/SplashActivity.kt
+++ app/src/main/java/com/domino/skhumap/activity/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.domino.skhumap.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.domino.skhumap.R
@@ -13,5 +15,14 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         val txtAnim = AnimationUtils.loadAnimation(this, R.anim.appear)
         splash_txt.startAnimation(txtAnim)
+        txtAnim.setAnimationListener(object:Animation.AnimationListener{
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                finish()
+            }
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+            override fun onAnimationStart(animation: Animation?) {}
+        })
     }
 }
